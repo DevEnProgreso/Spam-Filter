95c85127c859c6d57fe773e13e4d4479d286b64a
Frede <freddyzorio12@gmail.com>
2025-02-08 22:03:45 -0600

    second commit

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const checkMessageButton = document.getElementById("check-message-btn");
 
 const isSpam = (msg) => false;
 
-checkMessageButton.addEventListener("click", () => {
+heckMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
     alert("Please enter a message.");
     return;
   }
+  isSpam(messageInput.value)
+    ? (result.textContent = "Oh no! This looks like a spam message.")
+    : (result.textContent = "This message does not seem to contain any spam.");
+  messageInput.value = "";
 });
