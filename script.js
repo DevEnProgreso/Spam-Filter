cf8d06cab6a8107748e14e4d7e28b666477c0ec5
Frede <freddyzorio12@gmail.com>
2025-02-10 16:52:27 -0600

    final commit

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
-const isSpam = (msg) => false;
+const helpRegex = /please help|assist me/i;
+const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
+const freeRegex = /(?:^|\s)fr[e3][e3] m[o0]n[e3]y(?:$|\s)/i;
+const stockRegex = /(?:^|\s)[s5][t7][o0][c{[(]k [a@4]l[e3]r[t7](?:$|\s)/i;
+const dearRegex = /dear friend/i;
+const denyList = [helpRegex, dollarRegex, freeRegex, stockRegex, dearRegex];
 
-heckMessageButton.addEventListener("click", () => {
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
+
+checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
     alert("Please enter a message.");
     return;
   }
-  isSpam(messageInput.value)
-    ? (result.textContent = "Oh no! This looks like a spam message.")
-    : (result.textContent = "This message does not seem to contain any spam.");
+
+  result.textContent = isSpam(messageInput.value)
+    ? "Oh no! This looks like a spam message."
+    : "This message does not seem to contain any spam.";
   messageInput.value = "";
 });
